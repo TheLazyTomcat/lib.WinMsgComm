0a99dd1f18ee1b2b5a870b49085af60f9bb29701
František Milt <frantisek.milt@gmail.com>
2015-05-14 21:04:56 +0200

    Added license and copyright notices into code

--- WinMsgCommPeer.pas
+++ WinMsgCommPeer.pas
@@ -1,3 +1,21 @@
+{-------------------------------------------------------------------------------
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+-------------------------------------------------------------------------------}
+{===============================================================================
+
+  Windows Messages communication library
+
+  Peer endpoint class
+
+  ©František Milt 2015-05-14
+
+  Version 1.0
+
+===============================================================================}
 unit WinMsgCommPeer;
 
 interface
